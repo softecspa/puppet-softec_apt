fd6ff17868d88f87504177287a43778657df0d05
lorello <lorenzo.salvadorini@softecspa.it>
2015-09-17 15:40:11 +0200

    include apt in a better way

--- manifests/main_source_list.pp
+++ manifests/main_source_list.pp
@@ -1,33 +1,29 @@
 class softec_apt::main_source_list {
 
-  class {'apt':
-    always_apt_update     => true,
-    purge_sources_list_d  => true,
-    purge_sources_list    => true,
-    purge_preferences     => true,
-  }
+  include ::apt
 
-  apt::source {$::lsbdistcodename:
-    location  => 'http://archive.ubuntu.com/ubuntu/',
-    repos     => 'main restricted universe'
-  }
+  if $operatingsystem == 'Ubuntu' {
 
-  apt::source {"${::lsbdistcodename}-updates":
-    location  => 'http://archive.ubuntu.com/ubuntu/',
-    release   => "${::lsbdistcodename}-updates",
-    repos     => 'main restricted universe',
-  }
+      apt::source {$::lsbdistcodename:
+        location  => 'http://archive.ubuntu.com/ubuntu/',
+        repos     => 'main restricted universe'
+      }
 
-  apt::source {"${::lsbdistcodename}-security":
-    location  => 'http://security.ubuntu.com/ubuntu',
-    release   => "${::lsbdistcodename}-security",
-    repos     => 'main restricted universe',
-  }
+      apt::source {"${::lsbdistcodename}-updates":
+        location  => 'http://archive.ubuntu.com/ubuntu/',
+        release   => "${::lsbdistcodename}-updates",
+        repos     => 'main restricted universe',
+      }
 
-  apt::source {"${::lsbdistcodename}-partner":
-    location  => 'http://archive.canonical.com/ubuntu',
-    repos     => 'partner',
-  }
+      apt::source {"${::lsbdistcodename}-security":
+        location  => 'http://security.ubuntu.com/ubuntu',
+        release   => "${::lsbdistcodename}-security",
+        repos     => 'main restricted universe',
+      }
 
-
+      apt::source {"${::lsbdistcodename}-partner":
+        location  => 'http://archive.canonical.com/ubuntu',
+        repos     => 'partner',
+      }
+    }
 }
