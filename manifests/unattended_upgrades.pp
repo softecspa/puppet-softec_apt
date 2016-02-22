63711b65ffcdb78deb6faf815cd5bac4c5b550cf
lorello <lorenzo.salvadorini@softecspa.it>
2016-02-22 13:06:55 +0100

    dropping link to apt modules

--- manifests/unattended_upgrades.pp
+++ manifests/unattended_upgrades.pp
@@ -1,4 +1,4 @@
-class softec_apt::unattended_upgrades inherits apt::unattended_upgrades {
+class softec_apt::unattended_upgrades {
 
   File['/etc/apt/apt.conf.d/50unattended-upgrades'] {
     content => template('softec_apt/50unattended-upgrades.erb')
@@ -6,14 +6,6 @@ class softec_apt::unattended_upgrades inherits apt::unattended_upgrades {
 
   File['/etc/apt/apt.conf.d/10periodic'] {
     content => template('softec_apt/10periodic.erb')
-  }
-
-  # Se gli unattended-upgrades vengono installati da pacchetto manualmente, viene creato il file
-  # 20auto-upgrades. Poiche' il modulo apt di puppetlabs gestisce il tutto con il file 10periodic
-  # elimino il 20auto-upgrades ed utilizzero' solo il 10periodic
-
-  file {'/etc/apt/apt.conf.d/20auto-upgrades':
-    ensure  => absent,
   }
 
 }
