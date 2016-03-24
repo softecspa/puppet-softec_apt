785d6a9b2e3e964f5f0d7dc1f84452bb80e50810
lorello <lorenzo.salvadorini@softecspa.it>
2016-03-24 11:39:48 +0100

    Added ensure variable to proxy class

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,10 +1,12 @@
 class softec_apt::proxy (
+  $ensure     = present,
   $priority   = '02',
   $proxy      = 'http://packages.tools.softecspa.it',
   $port       = '3142'
 ) {
 
-  apt::conf{'proxy':
+  apt::conf { 'proxy':
+    ensure   => $ensure,
     priority => $priority,
     content  => "Acquire::http { Proxy \"${proxy}:${port}\"; };",
   }
